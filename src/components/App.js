ca9bde17d0865cdc949824c2dec71be4ead24c10
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 02:37:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { createContext, useState } from 'react';
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
+const UserContext = createContext();
 
 const App = () => {
-
-  return (
-    <div id="main">
-      
-        <UserProfile />
-      
-    </div>
-  )
-}
-
+	const [user, setUser] = useState({
+		name: 'Newton',
+		age: 3,
+	});
+	return (
+		<UserContext.Provider value={user}>
+			<div id="main">
+				<UserProfile />
+			</div>
+		</UserContext.Provider>
+	);
+};
 
 export default App;
-export {UserContext}
+export { UserContext };
