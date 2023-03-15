004604d79c0787e988755c926aa4a74e87e5e922
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 02:38:20 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,15 @@
-import { useContext } from 'react'
-import React from 'react'
-import { UserContext } from "./App"
+import React, { useContext } from 'react';
+import { UserContext } from './App';
 
-const UserProfile = () =>{
+const UserProfile = () => {
+	const { name, age } = useContext(UserContext);
 
-}
+	return (
+		<>
+			<h1 id="name">Name:- {name}</h1>
+			<h1 id="age">Age:- {age}</h1>
+		</>
+	);
+};
 
-export {UserProfile}
+export { UserProfile };
